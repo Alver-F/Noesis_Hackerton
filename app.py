d9d007974748c23410a6b4644195d481ba7e3e43
nishimay <nishiparodkar@gmail.com>
2024-11-10 18:04:12 +0530

    commit

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def homepage():
+    return render_template('index.html')
+
+@app.route('/rights')
+def rightspage():
+    return render_template('rights.html')
+
+@app.route('/councelling')
+def councellingpage():
+    return render_template('councelling.html')
+
+@app.route('/courses')
+def coursespage():
+    return render_template('courses.html')
+
+
+if '__main__' == __name__:
+    app.run(debug=True, port=5000)
+
+
